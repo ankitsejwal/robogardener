68b86d4b3231e11d922ef5c13f243ef988cd5e89
ankitsejwal <ankit@sejw.al>
2019-01-03 18:05:01 +1100

    start pump and tweet when plants are watered

--- gardener.py
+++ gardener.py
@@ -1,16 +1,49 @@
 import RPi.GPIO as GPIO
-import time
+import twitter
+import os, time
 
-# pin configurations
-LED_PIN = 1
 
-class Gardener(object):
+def start_pump(duration):
+    ''' run pump for a set interval '''
 
-    def __init__(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.output(PUMP, GPIO.HIGH)
+    time.sleep(duration)                   # keep pump running for 4 seconds
+    GPIO.output(PUMP, GPIO.LOW)
 
-    def set_led(self, value):
-        ''' Turn LED on/off '''
-        GPIO.output(LED_PIN, value)
+def send_tweet(mention):
+    
+    consumer_key = os.environ['twitter_consumer_key']
+    consumer_secret = os.environ['twitter_consumer_secret']
+    access_token_key = os.environ['twitter_access_token_key']
+    access_token_secret = os.environ['twitter_access_token_secret']
+
+    api = twitter.Api(consumer_key=consumer_key,
+                        consumer_secret=consumer_secret,
+                        access_token_key=access_token_key,
+                        access_token_secret=access_token_secret)
+
+    tweet = f'@{mention} The plants have been watered at ⏰{time.strftime("%H:%M")}'
+    
+    # post tweet
+    api.PostUpdate(status=tweet)
+
+
+if __name__ == "__main__":
+
+    PUMP = 17
+    BUTTON = 18
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(PUMP, GPIO.OUT)
+    GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+    # infinite loop
+    while True:
+        button_pressed = not GPIO.input(BUTTON)
+
+        if button_pressed:
+            start_pump(duration=4)                   # run pump for set interval time
+            send_tweet(mention='ankitsejwal')       # mention username to tag people in tweet
+        time.sleep(0.1)
